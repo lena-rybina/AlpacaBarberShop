8261309ad76ba61cc0b09bc03abeba076f850be5
Elena R <ielena.rybina@gmail.com>
2020-06-22 19:01:36 -0400

    Initial Commit

---
+++ AlpacaBarberShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlpacaBarberShop
+//
+//  Created by Vlad Zagorodnyuk on 6/22/20.
+//  Copyright © 2020 Elena Rybina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
