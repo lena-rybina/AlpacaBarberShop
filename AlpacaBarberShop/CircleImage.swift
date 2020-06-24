b63540d6283623c7d9b250cd111b238ed032ebe0
Elena R <ielena.rybina@gmail.com>
2020-06-24 15:35:18 -0400

    State changing

---
+++ AlpacaBarberShop/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  AlpacaBarberShop
+//
+//  Created by Vlad Zagorodnyuk on 6/23/20.
+//  Copyright © 2020 Elena Rybina. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct CircleImage: UIViewRepresentable {
+    typealias UIViewType = <#type#>
+    
+var body: some View {
+    Text("Hello World")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- AlpacaBarberShop/ContentView.swift
+++ AlpacaBarberShop/ContentView.swift
@@ -9,43 +9,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+    var state: AlpacaState = .notStarted
+    
+    var uiState: AlpacaUI {
+        switch state {
+            
+        case .notStarted: return AlpacaUI(topText: "Hello human! \nI need a shave!",
+                                          imageName: "notStarted",
+                                          buttonText: "Let's Go!")
+        case .inProgress: return  AlpacaUI(topText: "Shave more! \nI'm almost there",
+                                           imageName: "notStarted",
+                                           buttonText: "Shave more!")
+        default: return AlpacaUI(topText: "I'm smooth baby",
+                                 imageName: "shaved",
+                                 buttonText: "Thank You!")
+            
+        }
+    }
+  
     var body: some View {
         ZStack {
-           
-            Color("Mint")
-                .edgesIgnoringSafeArea(.all)
-           
-            VStack(alignment: .center) {
-                StrokeText(text: "Hello human! \nI need a shave!", width: 1, color: Color.black)
-                    .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                    .font(.largeTitle)
-                        .fixedSize(horizontal: false, vertical: true)
-                
-                Image("alpaca-1")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250)
-                    .cornerRadius(30)
-                .overlay(RoundedRectangle(cornerRadius: 30)
-                    .stroke(Color.white, lineWidth: 5
-                ))
-                .shadow(radius: 10)
-               
-                Button(action: {}) {
-                    Text("Start shaving")
-                        .font(.largeTitle)
-                        .padding()
-                    //.background(Color.pink)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color("YOrange"), Color("Peach")]), startPoint: .leading, endPoint: .trailing))
+                Color.white
+                VStack(alignment: .center) {
+                    StrokeText(text: "\(uiState.topText)", width: 1, color: Color.pink)
                         .foregroundColor(Color.white)
-                    .cornerRadius(20)
+                        .multilineTextAlignment(.center)
+                        .font(.largeTitle)
+                        .fixedSize(horizontal: false, vertical: true)
                     
+                    Image("\(uiState.imageName)")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                        .cornerRadius(30)
+                        .overlay(RoundedRectangle(cornerRadius: 30) .stroke(Color.white, lineWidth: 5))
+                        .shadow(radius: 10)
+                    
+                    
+                    Button(action: {print("Hey Listen!")}) {
+                        Text("\(uiState.buttonText)")
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .background(LinearGradient(gradient: Gradient(colors: [Color("YOrange"), Color("Peach")]), startPoint: .leading, endPoint: .trailing))
+                            .foregroundColor(Color.white)
+                            .cornerRadius(20)
+                            .shadow(radius: 10)
+                            .onTapGesture {
+                                self.state.shave()
+                                print("Tapped! - \(self.state)")
+                        }
+                    }
                 }
+                
+            }
+        }
+    }
+
+
+
+struct AlpacaUI {
+    var topText: String
+    var imageName: String
+    var buttonText: String
+}
+
+enum AlpacaState {
+    case shaved
+    case notStarted
+    case inProgress(Int)
+    
+    mutating func shave(with step: Int = 20) {
+        switch self {
+        case .shaved: break
+        case .notStarted:
+            self = .inProgress(0)
+        case .inProgress(let progress):
+            switch progress {
+            case Int.min..<0: self = .notStarted
+            case (100-step)...Int.max: self = .shaved
+            default: self = .inProgress(progress + step)
             }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
